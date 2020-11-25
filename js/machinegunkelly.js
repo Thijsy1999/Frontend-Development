f32899d6b9e31f0a77398152c754b02e0514dae5
Thijsy1999 <74351651+Thijsy1999@users.noreply.github.com>
2020-11-26 00:49:03 +0100

    Add files via upload

---
+++ js/machinegunkelly.js
@@ -0,0 +1,8 @@
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
